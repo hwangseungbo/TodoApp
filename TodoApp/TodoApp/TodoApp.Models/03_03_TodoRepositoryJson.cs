7b6f34c1482651e5f3fe6769803002a33fb6728b
황승보 <ghkdtmdqh@naver.com>
2021-11-19 10:43:31 +0900

    JSON을 C#으로 C#을 JSON으로 저장하기 데모

---
+++ TodoApp/TodoApp/TodoApp.Models/03_03_TodoRepositoryJson.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace TodoApp.Models
+{
+    public class TodoRepositoryJson : ITodoRepository
+    {
+        private readonly string _filePath;
+        private static List<Todo> _todos = new List<Todo>();
+
+        public TodoRepositoryJson(string filePath = @"C:\Users\SBHwang\source\repos\TodoApp\TodoApp\Todos.json")
+        {
+            this._filePath = filePath;
+            var todos = File.ReadAllText(filePath, Encoding.Default);
+            _todos = JsonConvert.DeserializeObject<List<Todo>>(todos);
+        }
+
+        public void Add(Todo model)
+        {
+            model.Id = _todos.Max(t => t.Id) + 1;
+            _todos.Add(model);
+
+            // JSON 파일 저장
+            string json = JsonConvert.SerializeObject(_todos, Formatting.Indented);
+            File.WriteAllText(_filePath, json);
+        }
+
+        public List<Todo> GetAll()
+        {
+            return _todos.ToList();
+        }
+    }
+}
+
